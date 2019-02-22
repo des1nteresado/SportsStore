b3dab2c5be61823d5ac8b8a8d5947f13a5e9cc6f
Daniil Chernyshev <des1nteresado@outlook.com>
2019-02-22 12:58:21 +0300

    add config files

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository rep)
+        {
+            repository = rep;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
